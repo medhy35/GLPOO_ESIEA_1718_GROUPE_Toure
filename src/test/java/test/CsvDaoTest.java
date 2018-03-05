913cc1bba93aad1f2bb8b11da6514cb7fe001de8
medhy35 <mahamadoun.toure1@gmail.com>
2018-03-05 23:15:30 +0100

    Ajout des premiers tests

---
+++ src/test/java/test/CsvDaoTest.java
@@ -0,0 +1,30 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class CsvDaoTest {
+	
+	// Chargement du fichier csv 
+	
+	@Before
+	public void doBefore() {
+		
+	}
+	
+	@Test
+	public void eclipseWorking() {
+
+		Assert.assertEquals(1, 1);
+	}
+	
+}
+
+
