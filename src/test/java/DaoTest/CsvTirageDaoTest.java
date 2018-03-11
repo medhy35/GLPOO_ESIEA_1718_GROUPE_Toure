98b8363a9ccb816dd63ac101381ca64bf2ca105c
medhy35 <mahamadoun.toure1@gmail.com>
2018-03-12 00:09:13 +0100

    Mise en place des premiers tests

---
+++ src/test/java/DaoTest/CsvTirageDaoTest.java
@@ -0,0 +1,31 @@
+package DaoTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import Dao.CsvTirageDao;
+
+public class CsvTirageDaoTest {
+	
+	private CsvTirageDao dao;
+	private final String link;
+	private final static String RESOURCES_PATH = "src/main/resources/";
+    private final static String LOTO_FILE_NAME = "euromillions.csv";
+	
+	public CsvTirageDaoTest() {
+		
+		link = RESOURCES_PATH + LOTO_FILE_NAME;
+		dao = new CsvTirageDao(link);
+	}
+	
+	
+	@Test
+	public void testfindAllTirages() {
+		final int referenceSize =151;
+		
+		final int rowSize = dao.findAllTirages().size();
+		
+		Assert.assertEquals(referenceSize,rowSize);
+	}
+	
+
+}
