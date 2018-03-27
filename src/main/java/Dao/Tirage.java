90202f6ad35c7211b20f1ff07b72e633e062c5da
medhy35 <mahamadoun.toure1@gmail.com>
2018-03-27 10:57:46 +0200

    Ajout du DAO

---
+++ src/main/java/Dao/Tirage.java
@@ -0,0 +1,25 @@
+package Dao;
+
+/**
+ * @author	Medhy35
+ * 
+ * 
+ * 
+ *
+ */
+public interface Tirage {
+	
+	int getBoule1();
+	
+	int getBoule2();
+	
+	int getBoule3();
+	
+	int getBoule4();
+	
+	int getBoule5();
+	
+	int getEtoile1();
+	
+	int getEtoile2();	
+}
