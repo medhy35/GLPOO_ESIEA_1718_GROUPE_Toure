2113edc96a7d5f1b188cc8bc313d7847284185bf
medhy35 <mahamadoun.toure1@gmail.com>
2018-03-27 11:02:12 +0200

    Ajout du launcher

---
+++ src/main/java/Launcher/Launcher.java
@@ -0,0 +1,20 @@
+package Launcher;
+import Swings.Welcome_interface;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * @author	Julie
+		 * 
+		 * Classe de lancement de l'application
+		 * @param	string args
+		 * @return	Ne retourne rien (methode void)	
+		 */
+		
+		new Welcome_interface();
+
+	}
+
+}
